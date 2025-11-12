c61ec7852026b404291228351adfc75c43f8994d
nftkungen <83460445+nftkungen@users.noreply.github.com>
2025-11-12 10:59:07 +0100

    Update server.js

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+app.use(express.static("public"));
 
 const app = express();
 const PORT = process.env.PORT || 8080;
